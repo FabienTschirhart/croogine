8e8a52b234afb4936e7dd96cf0feeea3d7e1dda1
Fabien Tschirhart <fabien.tschirhart@gmail.com>
2021-02-08 12:29:38 +0100

    creating vk instance

--- main.cpp
+++ main.cpp
@@ -17,6 +17,7 @@ public:
 private:
 
     GLFWwindow* window;
+    VkInstance instance;
     const int HEIGHT = 1280;
     const int WIDTH = 1024;
 
@@ -25,10 +26,11 @@ private:
         glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
         glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 
-        window = glfwCreateWindow(HEIGHT, WIDTH, "CrooLab Engine", nullptr, nullptr);
+        window = glfwCreateWindow(HEIGHT, WIDTH, "Croogine", nullptr, nullptr);
     }
 
     void initVulkan() {
+        createInstance();
 
     }
 
@@ -42,6 +44,36 @@ private:
         glfwDestroyWindow(window);
 
         glfwTerminate();
+    }
+
+    void createInstance() {
+
+        VkApplicationInfo appInfo{};
+        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        appInfo.pApplicationName = "Hello Croogine";
+        appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.pEngineName = "Croogine";
+        appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.apiVersion = VK_API_VERSION_1_0;
+
+        VkInstanceCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.pApplicationInfo = &appInfo;
+
+        uint32_t glfwExtensionCount = 0;
+        const char** glfwExtensions;
+
+        glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+        createInfo.enabledExtensionCount = glfwExtensionCount;
+        createInfo.ppEnabledExtensionNames = glfwExtensions;
+        createInfo.enabledLayerCount = 0;
+
+        VkResult result = vkCreateInstance(&createInfo, nullptr, &instance);
+
+        if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
+            throw std::runtime_error("failed to create instance!");
+        }
     }
 };
 
