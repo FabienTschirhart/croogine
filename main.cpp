feeefd2607e5e3398806bbb9070b1a8ced6278dc
Fabien Tschirhart <fabien.tschirhart@gmail.com>
2021-02-08 01:07:26 +0100

    native window

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
-#include <vulkan/vulkan.h>
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
 
 #include <iostream>
 #include <stdexcept>
@@ -7,22 +8,40 @@
 class HelloTriangleApplication {
 public:
     void run() {
+        initWindow();
         initVulkan();
         mainLoop();
         cleanup();
     }
 
 private:
+
+    GLFWwindow* window;
+    const int HEIGHT = 1280;
+    const int WIDTH = 1024;
+
+    void initWindow() {
+        glfwInit();
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+        window = glfwCreateWindow(HEIGHT, WIDTH, "CrooLab Engine", nullptr, nullptr);
+    }
+
     void initVulkan() {
 
     }
 
     void mainLoop() {
-
+        while (!glfwWindowShouldClose(window)) {
+            glfwPollEvents();
+        }
     }
 
     void cleanup() {
+        glfwDestroyWindow(window);
 
+        glfwTerminate();
     }
 };
 
