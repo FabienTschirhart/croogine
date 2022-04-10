bbdbe2490a6f782d659538da85ed19d85cba57bb
Fabien Tschirhart <fabien.tschirhart@gmail.com>
2022-04-10 22:13:04 +0100

    rendu 1

--- Croogine_VKUniformBuffer.cpp
+++ Croogine_VKUniformBuffer.cpp
@@ -25,7 +25,7 @@ void Croogine::updateUniformBuffer(uint32_t currentImage) {
 
     ubo.model = glm::rotate(glm::mat4(1.0f), time * glm::radians(20.f), glm::vec3(0.0f, 0.0f, 1.0f));
 
-    ubo.view = glm::lookAt(glm::vec3(-10.0f, 0.0f, 2.5f), glm::vec3(10.0f, 0.0f, 0.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+    ubo.view = glm::lookAt(glm::vec3(-8.0f, 0.0f, 2.8f), glm::vec3(10.0f, 0.0f, 0.0f), glm::vec3(0.0f, 0.0f, 1.0f));
     ubo.proj = glm::perspective(glm::radians(45.0f), swapChainExtent.width / (float)swapChainExtent.height, 0.1f, 100.0f);
     ubo.proj[1][1] *= -1;
 
