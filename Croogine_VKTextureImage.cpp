5bb5c3c479efe3c4b503dfaf6c1c5404708a8d68
Fabien Tschirhart <fabien.tschirhart@gmail.com>
2022-04-13 21:23:56 +0100

    lightmap 2048

--- Croogine_VKTextureImage.cpp
+++ Croogine_VKTextureImage.cpp
@@ -169,7 +169,7 @@ void Croogine::createTextureSampler() {
     samplerInfo.compareOp = VK_COMPARE_OP_ALWAYS;
 
     samplerInfo.mipmapMode = VK_SAMPLER_MIPMAP_MODE_LINEAR;
-    samplerInfo.minLod = static_cast<float>(mipLevels / 2);
+    samplerInfo.minLod = 0.0f;
     samplerInfo.maxLod = static_cast<float>(mipLevels);
     samplerInfo.mipLodBias = 0.0f;
 
