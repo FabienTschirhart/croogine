028d2ffc15ba0c03d6131b23fbdcf1a958820dd2
Fabien Tschirhart <fabien.tschirhart@gmail.com>
2022-06-25 21:47:36 +0100

    index n staging buffering

--- Croogine_Model.cpp
+++ Croogine_Model.cpp
@@ -27,17 +27,33 @@ namespace Croogine {
 		vertexCount = static_cast<uint32_t>(vertices.size());
 		assert(vertexCount >= 3 && "Vertex count must be at least 3");
 		VkDeviceSize bufferSize = sizeof(vertices[0]) * vertexCount;
+
+		VkBuffer stagingBuffer;
+		VkDeviceMemory stagingBufferMemory;
+
 		croogineDevice.createBuffer(
 			bufferSize,
-			VK_BUFFER_USAGE_VERTEX_BUFFER_BIT,
+			VK_BUFFER_USAGE_TRANSFER_SRC_BIT,
 			VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT,
+			stagingBuffer,
+			stagingBufferMemory);
+
+		void* data;
+		vkMapMemory(croogineDevice.getDevice(), stagingBufferMemory, 0, bufferSize, 0, &data);
+		memcpy(data, vertices.data(), static_cast<size_t>(bufferSize));
+		vkUnmapMemory(croogineDevice.getDevice(), stagingBufferMemory);
+
+		croogineDevice.createBuffer(
+			bufferSize,
+			VK_BUFFER_USAGE_VERTEX_BUFFER_BIT | VK_BUFFER_USAGE_TRANSFER_DST_BIT,
+			VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT,
 			vertexBuffer,
 			vertexBufferMemory);
 
-		void* data;
-		vkMapMemory(croogineDevice.getDevice(), vertexBufferMemory, 0, bufferSize, 0, &data);
-		memcpy(data, vertices.data(), static_cast<size_t>(bufferSize));
-		vkUnmapMemory(croogineDevice.getDevice(), vertexBufferMemory);
+		croogineDevice.copyBuffer(stagingBuffer, vertexBuffer, bufferSize);
+
+		vkDestroyBuffer(croogineDevice.getDevice(), stagingBuffer, nullptr);
+		vkFreeMemory(croogineDevice.getDevice(), stagingBufferMemory, nullptr);
 	}
 
 	void CroogineModel::createIndexBuffers(const std::vector<uint32_t>& indices) {
@@ -48,17 +64,32 @@ namespace Croogine {
 			return;
 
 		VkDeviceSize bufferSize = sizeof(indices[0]) * indexCount;
+		VkBuffer stagingBuffer;
+		VkDeviceMemory stagingBufferMemory;
+
 		croogineDevice.createBuffer(
 			bufferSize,
-			VK_BUFFER_USAGE_INDEX_BUFFER_BIT,
+			VK_BUFFER_USAGE_TRANSFER_SRC_BIT,
 			VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT,
+			stagingBuffer,
+			stagingBufferMemory);
+
+		void* data;
+		vkMapMemory(croogineDevice.getDevice(), stagingBufferMemory, 0, bufferSize, 0, &data);
+		memcpy(data, indices.data(), static_cast<size_t>(bufferSize));
+		vkUnmapMemory(croogineDevice.getDevice(), stagingBufferMemory);
+
+		croogineDevice.createBuffer(
+			bufferSize,
+			VK_BUFFER_USAGE_INDEX_BUFFER_BIT | VK_BUFFER_USAGE_TRANSFER_DST_BIT,
+			VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT,
 			indexBuffer,
 			indexBufferMemory);
 
-		void* data;
-		vkMapMemory(croogineDevice.getDevice(), indexBufferMemory, 0, bufferSize, 0, &data);
-		memcpy(data, indices.data(), static_cast<size_t>(bufferSize));
-		vkUnmapMemory(croogineDevice.getDevice(), indexBufferMemory);
+		croogineDevice.copyBuffer(stagingBuffer, indexBuffer, bufferSize);
+
+		vkDestroyBuffer(croogineDevice.getDevice(), stagingBuffer, nullptr);
+		vkFreeMemory(croogineDevice.getDevice(), stagingBufferMemory, nullptr);
 	}
 
 	void CroogineModel::draw(VkCommandBuffer commandBuffer){
