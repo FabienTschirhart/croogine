18fabe693dd10e43ac4a48fa170b1a475f23a963
Fabien Tschirhart <fabien.tschirhart@gmail.com>
2022-06-26 12:23:45 +0100

    tinyobj loader & tinygltf loader included

--- Croogine_Model.cpp
+++ Croogine_Model.cpp
@@ -1,7 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS
+
 #include "Croogine_Model.h"
 
 //std
 #include <vector>
+
+//lib
+#define TINYOBJLOADER_IMPLEMENTATION
+#define TINYGLTF_IMPLEMENTATION
+#define STB_IMAGE_IMPLEMENTATION
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+// #define TINYGLTF_NOEXCEPTION // optional. disable exception handling.
+
+#include <tiny_obj_loader.h>
+#include <tiny_gltf.h>
+
+using namespace tinygltf;
 
 
 namespace Croogine {
